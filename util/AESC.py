77e7fbc4df5161449065225627c5e932765d8dbf
周瑞斌 <ruibin.zhou@nascent.cn>
2025-03-03 22:35:40 +0800

    feat: Update aes method

--- util/AESC.py
+++ util/AESC.py
@@ -2,10 +2,10 @@ from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
 from cryptography.hazmat.backends import default_backend
 import base64
 from os import urandom
+from hashlib import sha256
 
 
 def _prepare_key(key: str) -> bytes:
-    from hashlib import sha256
     return sha256(key.encode('utf-8')).digest()
 
 
@@ -14,10 +14,12 @@ class AESCipher:
         self.key = _prepare_key(key)
 
     def encrypt(self, plaintext: str) -> str:
+        # 生成新的IV
         iv = urandom(16)
         cipher = Cipher(algorithms.AES(self.key), modes.CFB(iv), backend=default_backend())
         encryptor = cipher.encryptor()
         ciphertext = encryptor.update(plaintext.encode('utf-8')) + encryptor.finalize()
+        # 将IV和密文一起返回
         return base64.b64encode(iv + ciphertext).decode('utf-8')
 
     def decrypt(self, ciphertext_b64: str) -> str:
@@ -26,4 +28,5 @@ class AESCipher:
         real_ciphertext = data[16:]
         cipher = Cipher(algorithms.AES(self.key), modes.CFB(iv), backend=default_backend())
         decrypted = cipher.decryptor()
-        return decrypted.update(real_ciphertext).decode('utf-8') + decrypted.finalize().decode('utf-8')
+        decrypted_data = decrypted.update(real_ciphertext) + decrypted.finalize()
+        return decrypted_data.decode('utf-8')
